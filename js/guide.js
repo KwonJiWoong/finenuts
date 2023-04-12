29f19573de94d6b88c0d5d9b64c91a0667106707
KwonJiWoong <kjobaba@finenuts.co.kr>
2023-04-12 18:01:28 +0900

    tt

--- js/guide.js
+++ js/guide.js
@@ -4,10 +4,10 @@ $(function(){
         console.log(content);
         navigator.clipboard.writeText(content)
             .then(() => {
-            console.log("Text copied to clipboard...")
+            alert('클립보드에 복사되었습니다.')
         })
             .catch(err => {
-            console.log('Something went wrong', err);
+            console.log('복사에 실패하였습니다.', err);
         })
     });
 })
