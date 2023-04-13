1bd0afbb68e7d4894068c5cf997b6e2ba00ffca4
KwonJiWoong <kjobaba@finenuts.co.kr>
2023-04-13 13:23:31 +0900

    test

--- js/guide.js
+++ js/guide.js
@@ -1,7 +1,6 @@
 $(function(){
     $('.btn_copy').on('click',function(){
         var content = $(this).siblings('.hidden').text();
-        console.log(content);
         navigator.clipboard.writeText(content)
             .then(() => {
             alert('클립보드에 복사되었습니다.')
@@ -10,4 +9,38 @@ $(function(){
             console.log('복사에 실패하였습니다.', err);
         })
     });
-})
+
+
+
+    // 탭 초기화
+    function tab_init(){
+        if($('.tab-default').length > 0){
+            $('.tab-default').each(function(){
+                let $this = $(this).children('.selected');
+                let $width = $this.outerWidth();
+                let $left = $this.position().left;
+                if(!$(this).children('.bar').length > 0){
+                    $(this).append('<span class="bar"></span>');
+                }
+                $this.addClass('selected').siblings('.selected').removeClass('selected');
+                $this.siblings('.bar').stop().animate({
+                    'width' : $width,
+                    'left' : $left
+                },400);
+            });
+        }
+    }
+    // 탭 클릭 모션
+    $('.tab-default > button, .tab-default > a').on('click', function(e){
+        let $this = $(this);
+        let $width = $this.outerWidth();
+        let $left = $this.position().left;
+        e.preventDefault();
+        $this.addClass('selected').siblings('.selected').removeClass('selected');
+        $this.siblings('.bar').stop().animate({
+            'width' : $width,
+            'left' : $left
+        },400);
+    });
+    tab_init();
+});
