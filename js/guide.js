7733c971a820516246ef49f8fb16d0485d953511
KwonJiWoong <kjobaba@finenuts.co.kr>
2023-04-14 10:24:26 +0900

    코드 리팩토링 v01

--- js/guide.js
+++ js/guide.js
@@ -1,4 +1,49 @@
+let ui;
 $(function(){
+    ui = {
+        selectFunc : {
+            el : {
+                tabWrap : $('.tab-motion'),
+                tabBtn : $('.tab-motion').children('button, a')
+            },
+            selectTab : (element) => {      
+                element.addClass('selected').siblings('.selected').removeClass('selected');
+                ui.selectFunc.tabMotion(element);
+            },
+            tabMotion : (element) => {
+                const $bar = element.siblings('.bar');
+                const $width = element.outerWidth(true);
+                const $left = element.position().left;  
+                $bar.stop().animate({ 'left':$left, 'width':$width }, 400); 
+            },
+            initTabs : (element) => {
+                element.each(function(){
+                    const $selected = $(this).children('.selected');
+                    const $bar = $(this).children('.bar');
+                    /*  bar 없으면 추가 해주기 */
+                    if($bar.length < 1){
+                        $(this).append('<span class="bar"></span>');
+                    }
+                    ui.selectFunc.tabMotion($selected);
+                    ui.selectFunc.selectTab($selected);
+                });
+            },
+            clickEvt:(target)=> {
+                ui.selectFunc.selectTab(target);
+            },
+            test:()=>{console.log('test')},/* 테스트용 소스 */
+            init : () => {
+                ui.selectFunc.initTabs(ui.selectFunc.el.tabWrap);
+                $(ui.selectFunc.el.tabBtn).on('click', function(e){
+                    e.preventDefault();
+                    ui.selectFunc.clickEvt($(this));
+                });
+                
+            }
+        }
+    }
+
+    /* 클립보드 복사용 */
     $('.btn_copy').on('click',function(){
         var content = $(this).siblings('.hidden').text();
         navigator.clipboard.writeText(content)
@@ -9,41 +54,10 @@ $(function(){
             console.log('복사에 실패하였습니다.', err);
         })
     });
-
-
-
-    // 탭 초기화
-    function tab_init(){
-        if($('.tab-default, .tab-fill').length > 0){
-            $('.tab-default, .tab-fill').each(function(){
-                let $this = $(this).children('.selected');
-                let $width = $this.outerWidth(true);
-                let $left = $this.position().left;
-                if(!$(this).children('.bar').length > 0){
-                    $(this).append('<span class="bar"></span>');
-                }
-                $this.addClass('selected').siblings('.selected').removeClass('selected');
-                $this.siblings('.bar').stop().animate({
-                    'width' : $width,
-                    'left' : $left
-                },400);
-            });
-        }
-    }
-    function test(){
-        console.log('test Function 입니다.');
-    }
-    // 탭 클릭 모션
-    $('.tab-default > button, .tab-default > a, .tab-fill > button, .tab-fill > a').on('click', function(e){
-        let $this = $(this);
-        let $width = $this.outerWidth(true);
-        let $left = $this.position().left;
-        e.preventDefault();
-        $this.addClass('selected').siblings('.selected').removeClass('selected');
-        $this.siblings('.bar').stop().animate({
-            'width' : $width,
-            'left' : $left
-        },400);
-    });
-    tab_init();
+    /* //클립보드 복사용 */
+    ui.selectFunc.init();
 });
+
+$(document).ready(function(){
+    ui.selectFunc.test();
+})
